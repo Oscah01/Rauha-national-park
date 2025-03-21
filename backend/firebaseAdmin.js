c1a7740c77ed942942d335a9fbaae7e8f54a3412
Oscah <oscardeus1@yahoo.com>
2025-03-21 06:54:11 +0300

    add and solve admin functionality

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,8 +1,10 @@
 const admin = require('firebase-admin');
-require('dotenv').config();  // Load environment variables
+require('dotenv').config(); // Ensure dotenv is loaded
 
+// Parse the environment variable to get the service account credentials
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
